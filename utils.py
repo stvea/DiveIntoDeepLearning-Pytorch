8151c21cdf1b6a8586b4a15ecb70dfa59295e9bb
stvea <1191743792@qq.com>
2020-12-15 12:47:24 +0800

    3.2

---
+++ utils.py
@@ -0,0 +1,38 @@
+import random
+
+import torch
+from IPython import display
+from matplotlib import pyplot as plt
+
+
+def use_svg_display():
+    # 用矢量图显示
+    display.set_matplotlib_formats('svg')
+
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
+
+
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)  # 样本的读取顺序是随机的
+    for i in range(0, num_examples, batch_size):
+        j = torch.LongTensor(indices[i: min(i + batch_size, num_examples)])  # 最后一次可能不足一个batch
+        yield features.index_select(0, j), labels.index_select(0, j)
+
+
+def line_regression(X, w, b):
+    return torch.mm(X, w) + b
+
+
+def squared_loss(y_hat, y):
+    return (y_hat - y.view(y_hat.size())) ** 2 / 2
+
+
+def sgd(params, lr, batch_size):
+    for param in params:
+        param.data -= lr * param.grad / batch_size
